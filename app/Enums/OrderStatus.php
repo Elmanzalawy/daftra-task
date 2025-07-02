fa24c249b1a5f3ea7daa401a07262a1737d61312
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2025-07-02 20:03:12 +0300

    add Order model & migration

---
+++ app/Enums/OrderStatus.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enums;
+
+enum OrderStatus: string
+{
+    case Pending = 'pending';
+    case Processing = 'processing';
+    case Completed = 'completed';
+    case Cancelled = 'cancelled';
+
+    public static function toArray(): array
+    {
+        $result = [];
+        foreach (self::cases() as $case) {
+            $result[$case->value] = $case->name;
+        }
+
+        return $result;
+    }
+
+}
